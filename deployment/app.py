68e0ee88ee9ba33642b75901fba8d6afb04660dc
Gary Bair <49463909+garybair@users.noreply.github.com>
2023-02-27 15:02:22 -0500

    Modeling
    Completed:
    -Sentiment Analysis Modeling
    -Pickled XGB pipelines
    -Initial Flask Files

--- deployment/app.py
+++ deployment/app.py
@@ -1,31 +1,32 @@
 from flask import Flask, render_template, request, jsonify
-import pickle
-import pandas as pd
-import numpy as np
+import dill
+import os
+import sys
+sys.path.append('../src/')
 
 app = Flask(__name__)
+
+current_directory = os.getcwd()
+parent_directory = os.path.dirname(current_directory)
+models_folder = parent_directory + '/src/'
+
+#with open(models_folder + 'sia_pipeline.pkl', 'rb') as f:
+#    sentiment_model = dill.load(f)
     
 @app.route('/', methods=['GET', 'POST'])
-def home():
+def index():
     if request.method == 'POST':
-        text = request.form['text']
-        prediction = predict(text)
-        return render_template('result.html', prediction=prediction)
-    return render_template('index.html')
+        input_data = request.form['input_data']
+        main_varietal_prediction = main_varietal_model.predict([input_data])[0]
+        sub_varietal_prediction = sub_varietal_model.predict([input_data])[0]
+        sentiment_prediction = sentiment_model.predict([input_data])[0]
+        return render_template('result.html', 
+                               main_varietal = main_varietal_prediction, 
+                               sub_varietal = sub_varietal_prediction,
+                               sentiment = sentiment_prediction,)
+    else:
+        return render_template('landing_form.html')
     
     
 if __name__ == '__main__':
     app.run(debug=True)
-    
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>My Flask App</title>
-  </head>
-  <body>
-    <form method="POST" action="/">
-      <input type="text" name="text">
-      <button type="submit">Submit</button>
-    </form>
-  </body>
-</html>
