75d7125c7ff0ab9cc0694a37a6a4f518c1a8685c
Gary Bair <garybair3@gmail.com>
2023-02-27 21:47:01 -0500

    Bash

--- deployment/app.py
+++ deployment/app.py
@@ -1,12 +1,7 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
 import dill
-import os
-import sys
-sys.path.append('../src')
-import preprocessing
 import pandas as pd
-import numpy as np
-import traceback
+
 
 app = Flask(__name__)
 
@@ -18,22 +13,18 @@ with open('../models/sentiment_xgb_best_cv.pkl', 'rb') as f:
     
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    try:
-        if request.method == 'POST':
-            input_data = request.form['input_data']
-            input_data = [input_data]
-            input_data = pd.DataFrame(data = input_data, columns = ['reviewer_text'])
-            main_varietal_prediction = main_varietal_model.predict(input_data)
-            sentiment_prediction = sentiment_model.predict(input_data)
-            return render_template('result.html', 
-                                   main_varietal = str(main_varietal_prediction), 
-                                   sentiment = str(sentiment_prediction))
-        else:
-            return render_template('landing_form.html')
-
-    except Exception:
-        traceback.print_exc()
+    if request.method == 'POST':
+        input_data = request.form['input_data']
+        input_data = [input_data]
+        input_data = pd.DataFrame(data = input_data, columns = ['reviewer_text'])
+        main_varietal_prediction = main_varietal_model.predict(input_data)
+        sentiment_prediction = sentiment_model.predict(input_data)
+        return render_template('result.html', 
+                               main_varietal = main_varietal_prediction[0], 
+                               sentiment = sentiment_prediction[0])
+    else:
+        return render_template('index.html')
     
 if __name__ == '__main__':
-    app.run()
+    app.run(debug = True)
     
